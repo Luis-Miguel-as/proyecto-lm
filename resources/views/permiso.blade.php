aa980231fa274bbd4456c991d8228f3955696135
*LuisMiguelAS* <*wansthonlive@gmail.com*>
2019-09-24 03:57:24 -0500

    Trabajo con rutas

---
+++ resources/views/permiso.blade.php
@@ -0,0 +1,1 @@
+Sistema de permisos LM {{ $nombre }} {{ $slug }}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcomen');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('permiso', 'PermisoController@index');
+// Route::get('permiso/{nombre}/{slug?}', 'PermisoController@index');
+Route::get('permiso/{nombre}', function($nombre){
+return $nombre;
+})->where('nombre', '[A-Za-z]+')->name('permiso');
